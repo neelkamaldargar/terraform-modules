f87dbe97f506b506dc432af401aac3f9acd9975e
neelkamaldargar <85623866+neelkamaldargar@users.noreply.github.com>
2026-04-02 07:50:00 +0530

    Update main.tf

--- azure-rg-module/main.tf
+++ azure-rg-module/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.rg_neel_name
-  location = var.rg_neel_location
+  name     = var.rg_name
+  location = var.rg_location
 
   tags = {
     environment = "Dev"
